fa1398ef008907535b0e1415e955a5fc43627ce1
lokalmatador123 <38790188+lokalmatador123@users.noreply.github.com>
2020-12-13 13:36:07 +0100

    updates

--- concentrator_calculator/__main__.py
+++ concentrator_calculator/__main__.py
@@ -1,6 +1,8 @@
 import PySimpleGUI as sg
 import pandas as pd
 import pkg_resources
+import numpy as np
+from concentrator_calculator.time_calculator import calculate_time
 
 data = pkg_resources.resource_filename(__name__, 'data/centrifuge_data.xlsx')
 
@@ -14,28 +16,26 @@ def main():
     concentrator_types = infile.sheet_names
     infile.close()
 
-    ## collect liquid types
-    liquids = [column.split(' ')[0] for column in gui_data.columns.to_list()[2:]]
-
-    ## collect temperatures
+    ## collect liquids and temperatures
+    liquids = gui_data['Liquid'].unique()
     temps = gui_data['Temperature [°C]'].unique()
 
     ## layout definition
     layout = [
     [sg.Frame(layout = [
-    [sg.Radio(type, 'TYPE') for type in concentrator_types]],
+    [sg.Radio(type, "TYPE", key = '{}'.format(type)) for type in concentrator_types]],
     title = 'Select the type of concentrator')],
     [sg.Frame(layout = [
-    [sg.Radio(liquid, 'LIQUID') for liquid in liquids]],
+    [sg.Radio(liquid, 'LIQUID', key = '{}'.format(liquid)) for liquid in liquids]],
     title = 'Liquid to concentrate')],
     [sg.Frame(layout = [
-    [sg.Radio('{} °C'.format(temp), 'TEMP') for temp in temps]],
+    [sg.Radio('{} °C'.format(temp), 'TEMP', key = '{}'.format(temp)) for temp in temps]],
     title = 'Select a temperature')],
     [sg.Frame(layout = [
-    [sg.Text('Starting volume:'), sg.Spin([i for i in range(1, 2001)], size = (4, 1), key = 'STARTING_VOL')],
-    [sg.Text('Desired volume:'), sg.Spin([i for i in range(1, 2001)], size = (4, 1), key = 'DESIRED_VOL')]
-
-    ], title = 'test')]
+    [sg.Text('Starting volume:'), sg.Spin([i for i in range(1, 501)], size = (4, 1), key = 'STARTING_VOL')],
+    [sg.Text('Desired volume:'), sg.Spin([i for i in range(1, 501)], size = (4, 1), key = 'DESIRED_VOL')],
+    [sg.Text('', size = (20, 1), key = 'OUTPUT')]],
+    title = 'Volume information')]
     ]
 
     window = sg.Window('Concentrator calculator', layout)
@@ -46,6 +46,20 @@ def main():
         ## if x or exit are clicked, close the window
         if event == None or event == 'Exit':
             break
+
+        ## update the output element
+        ## seems there is no better way to get the keys than looping through them
+        func_params = [key for key in values.keys() if values[key]]
+
+        ## if all parameters are selected show output
+        ## and a valid input volume is selected
+        ## will be updated if more volume data is available
+        try:
+            if len(func_params) == 5:
+                window.Element('OUTPUT').Update('Estimated time: {}'.format(calculate_time(data, func_params[0], func_params[1], func_params[2], values['STARTING_VOL'], values['DESIRED_VOL'])))
+        except np.core._exceptions.UFuncTypeError:
+            if len(func_params) == 5:
+                window.Element('OUTPUT').Update('Select a valid volume.')
 
     window.close()
 
